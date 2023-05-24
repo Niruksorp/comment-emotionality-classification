2b48189ee3c288e954d915a949771ec7696c1555
niruksorp <niruksorpsfu@gmail.com>
2023-05-25 02:12:05 +0700

    Added save prepared dataset

--- src/util/database.py
+++ src/util/database.py
@@ -5,14 +5,16 @@ from psycopg2 import sql
 
 
 def connect_database():
-    conn = psycopg2.connect("postgres://admangarakov:zGKsv6rsX01790iaEivPCxc6TeC6cRvj@dpg-chn1bvak728vrd9a9ed0-a.oregon-postgres.render.com/aais")
+    conn = psycopg2.connect(
+        "postgres://admangarakov:zGKsv6rsX01790iaEivPCxc6TeC6cRvj@dpg-chn1bvak728vrd9a9ed0-a.oregon-postgres.render.com/aais")
     return conn
 
 
 def create_data_structure(connection):
     cursor = connection.cursor()
 
-    cursor.execute(sql.SQL("CREATE TABLE {table}(id int primary key, comment_message text, emotional_grade integer, version integer)")
+    cursor.execute(sql.SQL(
+        "CREATE TABLE {table}(id int primary key, comment_message text, emotional_grade integer, version integer)")
                    .format(table=sql.Identifier("dataset")))
 
     cursor.execute(sql.SQL(
@@ -31,12 +33,28 @@ def save_dataset(df, connection):
 
     values = []
     for index, row in df.iterrows():
-        values.append((row["Unnamed: 0"], row["Comment"], row["Sentiment"], "1"))
+        values.append((index, row["Comment"], row["Sentiment"], "1"))
 
     args = ','.join(cursor.mogrify("(%s,%s,%s,%s)", i).decode('utf-8')
                     for i in values)
 
     cursor.execute('INSERT INTO dataset (id, comment_message, emotional_grade, version) VALUES' + (args))
+
+    cursor.close()
+    connection.commit()
+
+
+def save_prepared_dataset(df, connection):
+    cursor = connection.cursor()
+
+    values = []
+    for index, row in df.iterrows():
+        values.append((index, row["CommentMessage"], row["Sentiment"]))
+
+    args = ','.join(cursor.mogrify("(%s,%s,%s)", i).decode('utf-8')
+                    for i in values)
+
+    cursor.execute('INSERT INTO preprocessed_dataset (id, comment_message, emotional_grade) VALUES' + (args))
 
     cursor.close()
     connection.commit()
