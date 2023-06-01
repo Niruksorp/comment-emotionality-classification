c15e9a8d0f98f28cbabd0dc045cf8b2f931064ad
niruksorp <niruksorpsfu@gmail.com>
2023-06-01 17:00:31 +0700

    Deploy journal init

---
+++ src/deploy.py
@@ -0,0 +1,9 @@
+from util.database import best_model
+
+
+def process():
+    best_model()
+
+
+if __name__ == "__main__":
+    process()
---
+++ src/foo.py
@@ -0,0 +1,9 @@
+from util.database import best_model_deploy
+
+
+def process():
+    best_model_deploy()
+
+
+if __name__ == "__main__":
+    process()
