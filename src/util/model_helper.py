8ac5be9ab8ad745cfc3da5bb07ed1dc3be7dc88e
Mangarakov Alexandr <ad.mangarakov@cdek.ru>
2023-06-01 16:35:06 +0700

    Implemented retraining pipeline

--- src/util/model_helper.py
+++ src/util/model_helper.py
@@ -1,3 +1,4 @@
+import io
 import torch
 
 PATH = "../entire_model.pt"
@@ -6,10 +7,15 @@ PATH = "../entire_model.pt"
 def prepare_model_to_save(model, name, score):
     # Save
     print(f'Start saving model with name: {name} and score: {score}')
-    torch.save(model, PATH)
-    byte_array = b''
-    with open(PATH, "rb") as f:
-        while byte := f.read(1):
-            byte_array += byte
+    model_out = io.BytesIO()
+    torch.save(model.state_dict(), model_out)
+    model_out.seek(0)
+    byte_array = model_out.read()
     print('Model saved')
     return byte_array, name, score
+
+
+def unpack_model(model, weights):
+    buffer = io.BytesIO(weights)
+    model.load_state_dict(torch.load(buffer))
+    return model
