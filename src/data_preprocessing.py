efc8739f9e76be3cf388ec99c5f55ffe3531e0eb
Mangarakov Alexandr <ad.mangarakov@cdek.ru>
2023-06-01 22:13:24 +0700

    fixup

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -31,7 +31,7 @@ def get_embs(batch):
 
 def main(timeout_min):
     # db.clear_preprocessed_data()
-    start_ind = db.get_start_ind_for_prepared_ds()
+    start_ind = db.get_start_ind_for_prepared_ds() + 1
     dataset = db.download_dataset(DATASET_TABLE_NAME)
     start_time = time.time()
     for start_pos in range(0, len(dataset), batch_size):
