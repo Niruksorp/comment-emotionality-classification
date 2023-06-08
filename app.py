278070af7b05751af8cfe00b5f0a2eee8ef8718e
niruksorp <niruksorpsfu@gmail.com>
2023-06-08 17:43:48 +0700

    Created API

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from flask import Flask, render_template
+from flask import Flask, request
 from src.data_preprocessing import get_embs
 from src.model.logistic_regression import create_model
 from src.model import perceptron
@@ -8,6 +8,7 @@ from src.util.database import best_model
 from src.util.model_helper import unpack_model
 import torch.nn.functional as F
 import numpy as np
+from torch.profiler import profile, record_function, ProfilerActivity
 
 app = Flask(__name__)
 
@@ -62,12 +63,22 @@ def get_emotional_grade(class_number):
     if class_number == 2:
         return 'positive'
 
+
 def eval(model, text):
     model.eval()
-    emb = get_embs([text])[0]
+    emb = get_embs([text])
     with torch.no_grad():
         res = model(emb)
+
+    with profile(activities=[ProfilerActivity.CPU],
+                 profile_memory=True, record_shapes=True) as prof:
+        loaded_model(emb)
+
+    print(prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=10))
+    print(prof.key_averages().table(sort_by="cpu_memory_usage", row_limit=10))
+
     return get_emotional_grade(np.argmax(res.numpy()))
+
 
 def get_model_by_name(name):
     if name == 'perceprton':
@@ -76,16 +87,22 @@ def get_model_by_name(name):
         return create_model()
     return None
 
-@app.route('/')
+
+@app.route('/', methods=['POST'])
 def home():
-    res, name = best_model()
-    model = get_model_by_name(name)
-    if model is None:
-        return 'BRUH'
-    model = unpack_model(model, res)
-    text = 'Fucking text'
-    print(f'This comment is {eval(model, text)}')
-    return render_template('index.html')
+    text = request.json['text']
+
+    return f'This comment is {eval(loaded_model, text)}'
+
+
+@app.route('/admin/loadModel/', methods=['GET'])
+def update_model():
+    global loaded_model, m_name
+    res, m_name = best_model()
+    model = get_model_by_name(m_name)
+    loaded_model = unpack_model(model, res)
+
+    return "Updated to version with" + m_name
 
 
 if __name__ == '__main__':
